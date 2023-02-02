554aa5f7cef3514278f34b1c290a40a59fe0f90c
mksaadi <mksaadi03@gmail.com>
2023-02-02 16:34:35 +0600

    Linked List Cycle

--- Leetcode/Linked List Cycle.cpp
+++ Leetcode/Linked List Cycle.cpp
@@ -1,59 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll=long long;
-using ull=unsigned  long long;
-using pi=pair<int,int>;
-using pl=pair<ll,ll>;
-using vi=vector<int>;
-using vl=vector<ll>;
-using vpi=vector<pi>;
-using vpl=vector<pl>;
-using ld = long double;
-#define ff first
-#define nl "\n" 
-#define ss second
-#define mp make_pair
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(),(x).rend()
-#define FastIO ios_base::sync_with_stdio(false); cin.tie(0);
-#define print(v) for(auto x:v){cout<<x<<" ";}cout<<"\n";
-const long long INF = 2000000000000000000LL;
-const long double EPS = 1e-9;
-const long long mod = 1e9+7;
-#define yes cout<<"YES\n"
-#define no cout<<"NO\n"
-#define set_precision(x) cout.setf(std::ios::fixed, std::ios::floatfield);cout.precision(x);
 
 struct ListNode {
-      int val;
+     int val;
      ListNode *next;
-     ListNode(int x) : val(x), next(NULL) {}
 };
 
 
-  bool hasCycle(ListNode *head) {
-        if(head==NULL)return false;
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while(slow!=NULL && fast!=NULL)
-        {
-            if(fast==slow)return true;
-            fast = fast->next;
-            slow = slow->next;
-            if(fast==NULL)return false;
-            fast = fast->next;
-        }
-        return false;
+bool hasCycle(ListNode *head) {
+    if(head==NULL)return false;
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+    while(slow!=NULL && fast!=NULL)
+    {
+        if(fast==slow)return true;
+        fast = fast->next;
+        slow = slow->next;
+        if(fast==NULL)return false;
+        fast = fast->next;
     }
+    return false;
+}
+void pushBack(int val,ListNode* Head)
+{
+
+    ListNode *new_node = new ListNode;
+    new_node->val = val;
+    new_node->next = NULL;
+
+
+    ListNode *a = Head;
+
+    if(Head==NULL)
+    {
+        Head = new_node;
+        return;
+    }
+
+    // go to the last node
+    while(1)
+    {
+        if(a->next==NULL)
+        {
+            a->next = new_node;
+            return;
+        }
+        a = a->next;
+    }
+
+}
+
+
 int main()
 {
- FastIO;
- int t;
- cin>>t;
- while(t--)
-  {
-     solve();
-  }
+ ListNode* Head = NULL;
+ int n;cin>>n;
+ for(int i=0;i<n;i++)
+ {  
+    int val; 
+    cin>>val;
+    pushBack(val,Head);
+ }
+ if(hasCycle(Head))
+ {
+    cout<<"HAS CYCLE\n";
+ }
+ else 
+ {
+    cout<<"NO CYCLE FOUND\n";
+ }
+
  return 0;
 }
