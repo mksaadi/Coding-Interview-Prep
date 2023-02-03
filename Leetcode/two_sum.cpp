7d278f110b72ad4d4f0457ed6eb1770e24610657
mksaadi <mksaadi03@gmail.com>
2023-02-03 16:34:53 +0600

    Solution to TwoSum

---
+++ Leetcode/two_sum.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using ull=unsigned  long long;
+using pi=pair<int,int>;
+using pl=pair<ll,ll>;
+using vi=vector<int>;
+using vl=vector<ll>;
+using vpi=vector<pi>;
+using vpl=vector<pl>;
+using ld = long double;
+#define ff first
+#define nl "\n" 
+#define ss second
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(0);
+#define print(v) for(auto x:v){cout<<x<<" ";}cout<<"\n";
+const long long INF = 2000000000000000000LL;
+const long double EPS = 1e-9;
+const long long mod = 1e9+7;
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define set_precision(x) cout.setf(std::ios::fixed, std::ios::floatfield);cout.precision(x);
+
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            int x = target-nums[i];
+            if(mp.find(x)!=mp.end())
+            {
+                return{mp[x],i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+}
+
+
+int main()
+{
+ FastIO;
+ int n;cin>>n;
+ int target;
+ cin>>target;
+ vi v(n);
+ for(int i=0;i<n;i++)
+ {
+    cin>>v[i];
+ }
+ print(twoSum(v,target));
+ return 0;
+}
